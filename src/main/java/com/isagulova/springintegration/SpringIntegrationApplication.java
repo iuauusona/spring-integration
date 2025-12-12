f8b852e6f11f24dc66a83a716a85beb29f7143bf
isagulovaburulsun05 <isagulovaburulsun05@gmail.com>
2025-12-12 11:34:07 +0600

    inner app integration

--- src/main/java/com/isagulova/springintegration/SpringIntegrationApplication.java
+++ src/main/java/com/isagulova/springintegration/SpringIntegrationApplication.java
@@ -2,12 +2,29 @@ package com.isagulova.springintegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+
+import java.util.Arrays;
 
 @SpringBootApplication
+@IntegrationComponentScan
 public class SpringIntegrationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringIntegrationApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(SpringIntegrationApplication.class, args);
+        Message<String> message = MessageBuilder
+                .withPayload("It is a body")
+                .setHeader("StringHeader", "Value")
+                .setHeader("IntHeader", 1)
+                .setHeader("ListOfStrings", Arrays.asList("String1", "String2"))
+                .build();
+
+        DirectChannel channel = context.getBean(DirectChannel.class);
+        channel.send(message);
     }
 
-}//fhfhhfddd
+}
---
+++ src/main/java/com/isagulova/springintegration/config/IntegrationConfig.java
@@ -0,0 +1,15 @@
+package com.isagulova.springintegration.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.channel.DirectChannel;
+
+@Configuration
+public class IntegrationConfig {
+
+    @Bean("channel")
+    public DirectChannel channel(){
+        return new DirectChannel();
+    }
+
+}
